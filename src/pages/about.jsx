75aade5ed769a0a2d562e234267932684bbe3388
Armando Perez <armandlord98@gmail.com>
2023-03-05 23:41:00 -0600

    agrega imagen en about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,13 +10,14 @@ import {
   GitHubIcon,
   LinkedInIcon,
 } from '@/components/SocialIcons'
-import portraitImage from '@/images/portrait.jpg'
+import portraitImage from '@/images/armex-about.png'
 
 function SocialLink({ className, href, children, icon: Icon }) {
   return (
     <li className={clsx(className, 'flex')}>
       <Link
         href={href}
+        target="_blank"
         className="group flex text-sm font-medium text-zinc-800 transition hover:text-sky-500 dark:text-zinc-200 dark:hover:text-sky-500"
       >
         <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-sky-500" />
@@ -41,11 +42,8 @@ export default function About() {
   return (
     <>
       <Head>
-        <title>About - Spencer Sharp</title>
-        <meta
-          name="description"
-          content="I’m Spencer Sharp. I live in New York City, where I design the future."
-        />
+        <title>About - Armex</title>
+        <meta name="description" content="Armex, un grupo de desarrolladores" />
       </Head>
       <Container className="mt-16 sm:mt-32">
         <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
@@ -61,59 +59,72 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Spencer Sharp. I live in New York City, where I design the
-              future.
+              ¿Qué es Armex?
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Maiores rerum voluptatem et. Obcaecati suscipit molestiae
+                blanditiis optio voluptate accusamus accusantium eum
+                voluptatibus beatae mollitia! Alias sed voluptatem quam ipsum
+                sit?
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Maiores rerum voluptatem et. Obcaecati suscipit molestiae
+                blanditiis optio voluptate accusamus accusantium eum
+                voluptatibus beatae mollitia! Alias sed voluptatem quam ipsum
+                sit?
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Maiores rerum voluptatem et. Obcaecati suscipit molestiae
+                blanditiis optio voluptate accusamus accusantium eum
+                voluptatibus beatae mollitia! Alias sed voluptatem quam ipsum
+                sit?
               </p>
               <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Maiores rerum voluptatem et. Obcaecati suscipit molestiae
+                blanditiis optio voluptate accusamus accusantium eum
+                voluptatibus beatae mollitia! Alias sed voluptatem quam ipsum
+                sit?
               </p>
             </div>
           </div>
           <div className="lg:pl-20">
             <ul role="list">
-              <SocialLink href="#" icon={TwitterIcon}>
+              {/* <SocialLink href="#" icon={TwitterIcon}>
                 Follow on Twitter
-              </SocialLink>
-              <SocialLink href="#" icon={InstagramIcon} className="mt-4">
+              </SocialLink> */}
+              <SocialLink
+                href="https://instagram.com/team_armex?igshid=MjkzY2Y1YTY="
+                icon={InstagramIcon}
+                className="mt-4"
+              >
                 Follow on Instagram
               </SocialLink>
-              <SocialLink href="#" icon={GitHubIcon} className="mt-4">
+              <SocialLink
+                href="https://github.com/ArmandLord"
+                icon={GitHubIcon}
+                className="mt-4"
+              >
                 Follow on GitHub
               </SocialLink>
-              <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
+              <SocialLink
+                href="https://www.linkedin.com/in/jos%C3%A9-armando-p%C3%A9rez/"
+                icon={LinkedInIcon}
+                className="mt-4"
+              >
                 Follow on LinkedIn
               </SocialLink>
-              <SocialLink
+              {/* <SocialLink
                 href="mailto:spencer@planetaria.tech"
                 icon={MailIcon}
                 className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
               >
                 spencer@planetaria.tech
-              </SocialLink>
+              </SocialLink> */}
             </ul>
           </div>
         </div>
